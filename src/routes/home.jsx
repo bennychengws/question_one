1e381fcefe3c63edabfbe8fbede65802d06f06e2
bennychengws <cwshing1@gmail.com>
2021-12-19 23:47:25 +0800

    modified

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -5,11 +5,11 @@ import moduleCss from "../home.module.css"
 const home = () => {
     return (
       <div className={moduleCss.container}>
-        <h1>
+        <h1 className={moduleCss.welcome}>
             Welcome Back!
         </h1>
         <nav>
-          <Link to="/home/trans_example_one">Translation example 1</Link> |{" "}
+          <Link to="/home/trans_example_one">Translation example 1</Link><div className={moduleCss.bar}>|</div>
           <Link to="/home/trans_example_two">Translation example 2</Link>
         </nav>     
         <Outlet />
